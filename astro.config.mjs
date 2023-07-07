9800f6ecd90aa4941aeedcd997bc111221d4a7c4
joshkaye88 <jkaye@shaw.ca>
2023-07-06 23:59:37 -0700

    update config, add img widths, delete unused images

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site : import.meta.env.PROD ? 'https://meganmriley.com/' : '',
+  site : import.meta.env.PROD ? 'https://meganmriley.com' : 'http://localhost:3000',
   base : import.meta.env.PROD ? '/join' : '/'
 });
