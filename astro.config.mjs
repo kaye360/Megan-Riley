e44ec3d6da4460fbdd983f68ec9f09e9aa7c8769
joshkaye88 <jkaye@shaw.ca>
2023-07-06 23:23:04 -0700

    add env vars to astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  // site : 'https://meganmriley.com/',
-  // base : '/join'
+  site : import.meta.env.PROD ? 'https://meganmriley.com/' : '',
+  base : import.meta.env.PROD ? '/join' : '/'
 });
